4b3e9fd272b365d9bedd55193628c9acf8144777
bibekjungthapa <bibekthapa007@hotmail.com>
2022-10-18 10:11:30 +0545

    Aunthentication

--- DisscussionNode/src/router/users.js
+++ DisscussionNode/src/router/users.js
@@ -13,7 +13,9 @@ router.post("/register", async (req, res) => {
       // console.log(hash)
       req.body['password'] = hash;
       console.log(req.body)
-       Users.create(req.body);
+       Users.create(req.body).then((data)=>{
+        console.log(data)
+       })
       // Store hash in your password DB.
   });
   } catch (error) {
@@ -29,16 +31,48 @@ router.post("/register", async (req, res) => {
 
   
 router.post("/login", async (req, res) => {
+  const userExistArr =Users.find({ password: req.body.password });
+  console.log(userExistArr)
+try {
 
-  console.log("hey hey")
-    const userExistArr = await Users.find({ email: req.body.email });
-    if (userExistArr.length > 0) {
-      res.json({
-        message: "USer USer found  found",
-      });
-    } else {
-      res.json({ message: "USerfound not found " });
-    }
+  bcrypt.compare(req.body.password, userExistArr.password, function(err, result) {
+    console.log(result)
+
+    // if (userExistArr.length > 0) {
+    //     res.json({
+    //       message: "USer USer found  found",
+    //     });
+    //   } else {
+    //     res.json({ message: "USerfound not found " });
+    //   }
+  
+});   
+
+} catch (error) {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // console.log("hey hey")
+  //   const userExistArr = await Users.find({ email: req.body.email });
+  //   if (userExistArr.length > 0) {
+  //     res.json({
+  //       message: "USer USer found  found",
+  //     });
+  //   } else {
+  //     res.json({ message: "USerfound not found " });
+  //   }
 });
   
   module.exports=router;
